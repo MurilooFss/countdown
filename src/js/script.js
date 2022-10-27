33cf9042d9969ca783c5f517b9ab1a5c286f97c3
MurilooFss <ferreira.murilosilva@outlook.com>
2022-10-27 12:20:25 -0300

    Adicionada funcionalidade do contador e alterações no layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,24 @@
 function calcTime(){
-    let date = new Date(12/12/2022)
-    let eventDay = 12
-    let eventMonth = 12
-    let thisDay = date.getDate()
-    let thisHour = date.getHours()
-    let thisMin = date.getMinutes()
-    let thisSec = date.getSeconds()
-    console.log(date)
-    
+    const date = new Date().getTime()
+    const eventDay = new Date('oct 31, 2022 11:00:00').getTime();
+    const today = eventDay - date
+    let thisDay = Math.floor(today / (1000*60*60*24))
+    let thisHour = Math.floor((today % (1000*60*60*24))/(1000*60*60))
+    let thisMin = Math.floor((today % (1000*60*60))/(1000*60))
+    let thisSec =Math.floor((today % (1000*60))/(1000))
+    let space = ' '
+    if(today < 0){
+        document.querySelector('.countdown').innerHTML=('<h1>Evento Finalizado!!!</h1><h3>Fique Ligado nas nossas redes sociais para saber as próximas datas!</h3>')
+    }
+
+    document.querySelector('#secondsTime').textContent=(space+ addZero(thisSec))
+    document.querySelector('#minutesTime').textContent=(space+addZero(thisMin))
+    document.querySelector('#hoursTime').textContent=(space+addZero(thisHour))
+    document.querySelector('#daysTime').textContent=(space+addZero(thisDay))  
+}
+function addZero(value){
+    x = value <10 ? '0' + value : value
+    return x
 }
 calcTime()
+setInterval(calcTime, 1000)
