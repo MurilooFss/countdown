0ffb6a9a10e78e274109c503d9dd23b723823f64
murilo ferreira <ferreira.murilosilva@outlook.com>
2022-10-26 21:50:39 -0300

    Countdown

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+function calcTime(){
+    let date = new Date(12/12/2022)
+    let eventDay = 12
+    let eventMonth = 12
+    let thisDay = date.getDate()
+    let thisHour = date.getHours()
+    let thisMin = date.getMinutes()
+    let thisSec = date.getSeconds()
+    console.log(date)
+    
+}
+calcTime()
